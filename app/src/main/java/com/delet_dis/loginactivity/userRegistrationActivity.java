b509bc6199af188809a9e437c21ef77bb0ea8ab0
Igor Efimov <efimov.036@gmail.com>
2020-11-16 17:35:54 +0700

    added new layout

---
+++ app/src/main/java/com/delet_dis/loginactivity/userRegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.delet_dis.loginactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userRegistrationActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_user_registration);
+  }
+}
